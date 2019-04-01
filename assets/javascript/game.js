f03e2700a8b9de3c9412d9d8def09a3edf00f9c3
Lilu <KHussell@gmail.com>
2019-04-01 16:33:53 -0400

    enemies append to enemies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,135 @@
-var offenseChar,
+var offenseChar = "",
     offenseAttackPower,
     offenseHealthPoints,
-    defenseChar,
+    defenseChar="",
     defenseCounterAttackPower,
     defenseHealthPoints
 
-var character1= $("#character1"), //contains name, picture, empty p for inital HP
-    character2= $("#character2"),
-    character3= $("#character3"),
-    character4= $("#character4"),
-    initialhP1Text = $("#hp1").text(),
-    initialhP2Text = $("#hp2").text(),
-    initialhP3Text = $("#hp3").text(),
-    initialhP4Text = $("#hp4").text(),
+var yoda= $("#character1"), //contains name, picture, empty p for inital HP
+    luke= $("#character2"),
+    obi= $("#character3"),
+    mace= $("#character4"),
     enemies = $(".enemies"), //empty div for enemies to be placed
     youImage= $("#youImage"),
     youHP= $("#youHP"),
     youAttack= $("#youAttackPower"),
     defenderImage= $("#defenderImage"),
     defenderHP= $("#defenderHP"),
-    defenderCounter= ("#defenderCounterAttack")
+    defenderCounter= $("#defenderCounterAttack")
 
-var characters= {
-      character1: {
+var characters= [
+       {
+          name: "Yoda",
           hP: 150,
           initialAttackPower: 9,
           counterAttackPower: 13
       },
-      character2: {
-          hP: 130,
-          initialAttackPower: 8,
-          counterAttackPower: 11
-      },
-      character3: {
+       {
+          name: "Luke Skywalker",
           hP: 100,
           initialAttackPower: 5,
           counterAttackPower: 7
       },
-      character4: {
+       {
+          name: "Obi Wan Kenobi",
+          hP: 130,
+          initialAttackPower: 8,
+          counterAttackPower: 11
+      },
+       {
+          name: "Mace Windu",
           hP: 120,
           initialAttackPower: 7,
           counterAttackPower: 10
       }
-}
+    ]
+
+
+var character1= characters[0],
+    character2= characters[1],
+    character3= characters[2],
+    character4= characters[4]
+
+
+
 
 
 var game={
-
+   
 }
 
 
 
-document.ready
+$(document).ready(function(){
+
+
+  $("#hP1").text(characters[0]['hP'])
+  $("#hP2").text(characters[1]['hP'])
+  $("#hP3").text(characters[2]['hP'])
+  $("#hP4").text(characters[3]['hP'])
+
+
+
+
+yoda.on("click", function(){
+  if (offenseChar === ""){
+    offenseChar= character1
+    defenseChar= characters.filter(function(item){
+      return item.name !== "Yoda"
+    })
+    $(".enemies").append(luke)
+    $(".enemies").append(obi)
+    $(".enemies").append(mace)  
+} 
+})
+
+
+
+luke.on("click", function(){
+  if(offenseChar === ""){
+    offenseChar= character2
+    defenseChar= characters.filter(function(item){
+      return item.name !== "Luke Skywalker"
+      })
+    $(".enemies").append(yoda)
+    $(".enemies").append(obi)
+    $(".enemies").append(mace)
+  }
+})
+
+
+obi.on("click", function(){
+  if(offenseChar === ""){
+    offenseChar= character3
+    defenseChar= characters.filter(function(item){
+      return item.name !== "Obi Wan Kenobi"
+    })
+    $(".enemies").append(yoda)
+    $(".enemies").append(luke)
+    $(".enemies").append(mace)
+  }
+})
+
+
+
+mace.on("click", function(){
+  if(offenseChar === 0){
+    offenseChar= character4
+    defenseChar= characters.filter(function(item){
+      return item.name !== "Mace Windu"
+    })
+    $(".enemies").append(yoda)
+    $(".enemies").append(luke)
+    $(".enemies").append(obi)
+  }
+})
+
+
+
+
+
+
+
+
+
+})
